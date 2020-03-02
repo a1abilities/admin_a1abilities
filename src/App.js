5255c99164341c8881fc792b8e4898c3f7a82df4
kamleshgehlot <kamlesh.gehlott@gmail.com>
2020-03-02 10:45:59 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Suspense, lazy} from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-
+import ReactDom from 'react-dom';
 
 
 import PageLoader from './common/PageLoader';
